92fb063f91a7f691aa39d8717d04e5e0fd26b723
VladislavBukreev <bukr7@mail.ru>
2023-02-11 16:38:22 +1000

    Step_1_Completed

--- 4_Max_Int_number/Program.cs
+++ 4_Max_Int_number/Program.cs
@@ -1,3 +1,37 @@
 ﻿// Максимальное целое из трёх чисел
-Console.WriteLine ("Введите чило 1");
-int numberA = Console.ReadLine();
+Console.WriteLine("Введите число 1: ");
+int numberA = int.Parse(Console.ReadLine());
+Console.WriteLine("Введите число 2 ");
+int numberB = int.Parse(Console.ReadLine());
+Console.WriteLine("Введите число 3: ");
+int numberC = int.Parse(Console.ReadLine());
+int max = 0;
+if (numberA>numberB)
+{
+    max = numberA;
+    if (numberA>numberC)
+    {
+        max = numberA;
+    }
+    else 
+    {
+        max = numberC;
+    }
+}
+       
+ else 
+ {
+    max = numberB;
+        if (numberB>numberC)
+        {
+            max = numberB;
+        }
+        else 
+        {
+            max = numberC;
+        }
+        
+ }
+
+Console.WriteLine("Максимальное число это: ");
+Console.WriteLine(max);
