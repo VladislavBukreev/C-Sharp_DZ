d01d2d9ecb6e410393beca3c81a390ebd727c396
VladislavBukreev <bukr7@mail.ru>
2023-02-11 17:09:17 +1000

    Задача_2_Completed

---
+++ 6_Деление_без_остатка/Program.cs
@@ -0,0 +1,11 @@
+﻿// Делим без остатка
+Console.WriteLine("Ведите число ");
+int x = int.Parse(Console.ReadLine());
+if(x % 2==0)
+{
+    Console.WriteLine("Это число четное");
+}
+else
+{
+    Console.WriteLine("Это число НЕчетное");
+}
