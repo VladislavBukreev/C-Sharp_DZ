c2b405661137062fa555449e5ea7e33aa03d1f2a
VladislavBukreev <bukr7@mail.ru>
2023-02-18 15:22:24 +1000

    Домашние задание к 2 семинару

---
+++ Task_13/Program.cs
@@ -0,0 +1,11 @@
+﻿//Задача 13: Напишите программу, которая выводит третью цифру заданного числа или сообщает, что третьей цифры нет.
+Console.Clear();
+Console.WriteLine("Как всегда - введите число");
+string a = Console.ReadLine();
+if(a.Length < 3)
+    {Console.WriteLine("Третьей цыфры нет "); 
+    return;
+    }
+
+Console.WriteLine($"Число третье по счёту это -> {a[2]}");
+
---
+++ Task_15/Program.cs
@@ -0,0 +1,39 @@
+﻿//Напишите программу, которая принимает на вход цифру, обозначающую день недели, и проверяет, является ли этот день выходным.
+
+Console.Clear();
+int Prompt(string message)
+{
+    Console.Write(message);
+    string value = Console.ReadLine();                  //Метод, в котороом просят ввести день недели и конвертируют в тип int, запоминают это как result 
+    int result = Convert.ToInt32(value); 
+    return result;
+}
+bool IsWeekend(int weekDay)
+{ 
+        if (weekDay > 5) 
+        {   
+            return true;                            // Метод, в котором выявляется выходной  (6 и 7 день)
+        } 
+        return false;
+}
+bool ValidateWeekday(int number)
+{
+    if (number > 0 && number <= 7)
+    {
+         return true;                                     // Метод, в котором проверяют что введёное число это день недели а не больше.
+    }
+    Console.WriteLine("Это не день недели!"); 
+    return false;
+}
+int weekDay = Prompt("Введите день недели >");             // Начало операции, которое присваивает переменной weekDay значение result
+if (ValidateWeekday(weekDay))  
+{
+    if (IsWeekend(weekDay))
+    { 
+    Console.WriteLine("Наконец-то выходной");
+    }                                                       
+    else 
+    {
+        Console.WriteLine("Придется поработать");
+    }
+}
