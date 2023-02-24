00729b4e24325e225d54dc7471f5e9b7b1f0dc9e
VladislavBukreev <bukr7@mail.ru>
2023-02-25 01:34:28 +1000

    task_27

---
+++ Task_27/Program.cs
@@ -0,0 +1,19 @@
+﻿//Задача 27: Напишите программу, которая принимает на вход число и выдаёт сумму цифр в числе.
+//85 -> 13
+
+
+int Summ(int n){
+int result = 0;
+while (n > 0)
+{
+    result = result + (n % 10);
+    n = n / 10;
+}
+return result;
+}
+
+
+Console.Clear();
+Console.WriteLine("Введите число: ");
+int n = int.Parse(Console.ReadLine());
+Console.WriteLine($"Сумма всех введеных чисел равна -> {Summ(n)}");
