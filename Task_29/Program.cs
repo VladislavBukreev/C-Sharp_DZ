978f7288215ebbe961ff61bf79180f6cddb1e350
VladislavBukreev <bukr7@mail.ru>
2023-02-25 14:07:20 +1000

    task_29

---
+++ Task_29/Program.cs
@@ -0,0 +1,16 @@
+﻿// Задача 29: Напишите программу, которая задаёт массив из 8 элементов и выводит их на экран.
+
+int[] NewArray(int size){
+    int[] result = new int[size];
+    for (int i = 0; i < size; i++) 
+    {
+        result[i] = new Random().Next(1,999);
+    }
+    return result;
+}
+
+int[] array = NewArray(8);
+
+Console.Clear();
+
+Console.WriteLine($"Генерация нового массива из 8 элментов ... [    {String.Join(", ", array)}     ]");
