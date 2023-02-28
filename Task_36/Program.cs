5a3b2c75015c97eafa2b905258c2f34a9637aa6f
VladislavBukreev <bukr7@mail.ru>
2023-02-28 21:55:34 +1000

    Task_36

--- Task_36/Program.cs
+++ Task_36/Program.cs
@@ -6,15 +6,15 @@ int size = int.Parse(Console.ReadLine());
 Console.WriteLine("Какое зададим максимальное рандомное число в массиве ? -> ");
 int random = int.Parse(Console.ReadLine());
 int[] array = NewArray(size);
-Console.WriteLine($"Создаю случайный массив ... [    {String.Join(" , " , array)}    ]");
-int sum = 0;
+Console.WriteLine($"Создаю случайный массив ... [    {String.Join(" , ", array)}    ]");
+
 Console.WriteLine($"Сумма элементов на нечетной позиции равна -> {Summ(array)}");
 
 
 int[] NewArray(int size)
 {
     int[] mas = new int[size];
-    for(int i = 0; i < size; i++)
+    for (int i = 0; i < size; i++)
 
         mas[i] = new Random().Next(random);
     return mas;
@@ -23,10 +23,14 @@ int[] NewArray(int size)
 
 int Summ(int[] NewArray)
 {
-    for(int i = 0; i < array.Length; i++)
-    if( i % 2 != 0 )
+    int sum = 0;
+    for (int i = 0; i < NewArray.Length; i++)
     {
-        sum =+ array[i];
+        
+        if (i % 2 != 0)
+        {
+            sum += NewArray[i];
+        }
     }
     return sum;
 }
