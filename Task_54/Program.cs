70174d4d46eac6df8aef09607f56a750c7d0aa1c
VladislavBukreev <bukr7@mail.ru>
2023-03-10 19:21:31 +1000

    Task_54_Sortirovka_Mass

---
+++ Task_54/Program.cs
@@ -0,0 +1,69 @@
+﻿/*Задача 54: Задайте двумерный массив. Напишите программу, которая упорядочит по убыванию элементы каждой строки двумерного массива.
+Например, задан массив:
+1 4 7 2
+5 9 2 3
+8 4 2 4
+В итоге получается вот такой массив:
+7 4 2 1
+9 5 3 2
+8 4 4 2
+*/
+Console.Clear();
+Console.WriteLine("Введите размер строк массива -> ");
+int rows = int.Parse(Console.ReadLine());
+Console.WriteLine("Введите размер столбцов массива -> ");
+int columns = int.Parse(Console.ReadLine());
+int [,] array = GetArray(rows, columns , 0,10);
+PrintArray(array);
+System.Console.WriteLine();
+SortingArray(array);
+PrintArray(array);
+int[,] GetArray(int n , int m, int minValue , int maxValue)
+{
+    int[,] result = new int [n,m];
+    for(int i = 0; i < n ; i++)
+    {
+        for(int j = 0; j < m; j++)
+        {
+            result[i,j] = new Random().Next (minValue, maxValue);
+        }
+    }
+    return result;
+}
+
+void PrintArray(int [,] array)
+{
+     for(int i = 0; i < array.GetLength(0); i++)
+    {
+        for(int j = 0; j < array.GetLength(1); j++)
+        {
+            Console.Write($" {array[i,j]} ");
+        }
+        Console.WriteLine();
+    }
+    
+}
+
+//метод сортировки пузырьком
+void SortingArray(int[,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            for (int k = 0; k < array.GetLength(1) - 1; k++)
+            {
+                if (array[i, k] < array[i, k + 1])
+                {
+                    int temp = array[i, k + 1];
+                    array[i, k + 1] = array[i, k];
+                    array[i, k] = temp;
+                }
+            }
+        }
+    }
+}         
+            
+        
+        
+  
